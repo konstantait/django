ece6f0c1e31174ad72def91f82326eb3e16af3df
John Doe <konstanta.it@gmail.com>
2023-03-28 20:37:18 +0300

    main project structure

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import *
+from store.models import Product
 # Register your models here.
 
 admin.site.register(Product)
--- store/models.py
+++ store/models.py
@@ -4,9 +4,10 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=200)
-    price = models.FloatField()
-    image = models.ImageField(null=True, blank=True)
-
-    def __str__(self):
-        return self.name
+    pass
+#     name = models.CharField(max_length=200)
+#     price = models.FloatField()
+#     image = models.ImageField(null=True, blank=True)
+#
+#     def __str__(self):
+#         return self.name
