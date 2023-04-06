68fe73b6360e749dfea33e716c0fb803500a9029
John Doe <konstanta.it@gmail.com>
2023-04-06 18:26:52 +0300

    main project structure

---
+++ catalog/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.html import strip_tags
+
+from catalog.models import Review
+
+
+class ReviewModelForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ('author', 'product', 'rating', 'text')
+
+    def __init__(self, author, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['author'].widget = forms.HiddenInput()
+        self.fields['author'].initial = author
+
+    def clean_text(self):
+        data = strip_tags(self.cleaned_data['text'])
+        if not data:
+            raise forms.ValidationError("Text field empty after clearing html-tags")
+        return data
--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# from .models import Product
+from catalog.models import Review
+from catalog.forms import ReviewModelForm
 
 
-def catalog(request):
-    # products = Product.objects.all()
-    # context = {'products': products}
+def home(request):
     context = {}
     return render(request, 'catalog/index.html', context)
+
+
+@login_required
+def reviews(request):
+    author = request.user
+    form = ReviewModelForm(author=author)
+    if request.method == 'POST':
+        form = ReviewModelForm(author=author, data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Thank you for review!')
+            return redirect('home')
+        else:
+            messages.error(request, 'Check input')
+    else:
+        messages.info(request, f'Welcome {author}, leave your review')
+    context = {
+        'form': form,
+        'reviews': Review.objects.all()
+    }
+    return render(request, 'catalog/reviews.html', context=context)
