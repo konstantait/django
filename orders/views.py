979264cb0cdd78eb5ff36945885a52f6d0de1ad8
konstantait <111305936+konstantait@users.noreply.github.com>
2023-04-27 17:47:47 +0300

    Update views.py

--- orders/views.py
+++ orders/views.py
@@ -22,7 +22,7 @@ def order_create(request):
                     price=item['price'],
                     quantity=item['quantity'])
             order.save()
-            # cart.clear()
+            cart.clear()
             # asynchronous task send mail
             # order_created(order.id)
             return redirect('orders:payment', pk=order.id)
